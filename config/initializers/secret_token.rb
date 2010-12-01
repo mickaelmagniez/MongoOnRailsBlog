ac1e68fde013d4fbb56de1cc7ad4fc3c837c7c6b
mickael <mickael@xubuntu.(none)>
2010-12-01 09:09:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoOnRailsBlog::Application.config.secret_token = '0015cda8a69ffc9ca2cf0774dfc990816ba7dca69e1bc059968ebca427a703988b5e3b36c6768a6375ff3117bc79b86819c3dcb608382afa185d6db01af78573'
